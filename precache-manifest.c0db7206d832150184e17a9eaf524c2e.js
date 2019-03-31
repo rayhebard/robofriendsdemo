14e06d575c56bdf1559affb8fa7a3a8f8684e3c3
Random Hebard <rayhebard@gmail.com>
2019-03-31 19:46:21 -0400

    Updates

---
+++ precache-manifest.c0db7206d832150184e17a9eaf524c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfa1f060250d0414683e",
+    "url": "/rayhebard/robofriendsdemo.git/static/js/runtime~main.cfa1f060.js"
+  },
+  {
+    "revision": "f2a533741dbcd7a56d77",
+    "url": "/rayhebard/robofriendsdemo.git/static/js/main.f2a53374.chunk.js"
+  },
+  {
+    "revision": "afdf85d41b77ee17b845",
+    "url": "/rayhebard/robofriendsdemo.git/static/js/1.afdf85d4.chunk.js"
+  },
+  {
+    "revision": "f2a533741dbcd7a56d77",
+    "url": "/rayhebard/robofriendsdemo.git/static/css/main.4321b672.chunk.css"
+  },
+  {
+    "revision": "afdf85d41b77ee17b845",
+    "url": "/rayhebard/robofriendsdemo.git/static/css/1.cb27dac3.chunk.css"
+  },
+  {
+    "revision": "6e810ad4bc2b4f76984703b7400aee60",
+    "url": "/rayhebard/robofriendsdemo.git/index.html"
+  }
+];
